c2bb97ddeee4330f5b7a78f8e47da9f0fea01b4c
ImJaePang <narushel@naver.com>
2025-02-13 22:04:33 +0900

    noneedUseEffect

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,17 @@ import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
 import logoImg from "./assets/logo.png";
 import { sortPlacesByDistance } from "./loc.js";
 
+const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+const storedPlaces = storedIds.map(id => AVAILABLE_PLACES.find((place)=> place.id === id));
+
 function App() {
+
     const modal = useRef();
     const selectedPlace = useRef();
     const [availablePlaces, setAvailablePlaces] = useState([]);
-    const [pickedPlaces, setPickedPlaces] = useState([]);
+    const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
+
+
 
     useEffect(() => {
         navigator.geolocation.getCurrentPosition((position) => {
@@ -59,6 +65,9 @@ function App() {
             )
         );
         modal.current.close();
+
+        const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+        localStorage.setItem('selectedPlaces', JSON.stringify(storedIds.filter((id)=> id !== selectedPlace.current)))
     }
 
     return (
